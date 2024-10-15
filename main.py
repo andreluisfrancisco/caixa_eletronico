61f5aec649188ec2ddfad7c76467af597da72c61
André Luis Francisco <andreluis.francisco@gmail.com>
2024-10-15 17:21:20 -0300

    Refatora o main.py

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from controllers.atmControllers import CaixaEletronico
+MENU_ENTRADA = 'e'
+MENU_SAIR = 's'
+
+def exibir_mensagem_boas_vindas():
+    print("\nBem-vindo ao Caixa Eletrônico!")
+
+def exibir_mensagem_despedida():
+    print("Obrigado por usar o caixa eletrônico.")
+
+def obter_opcao_usuario():
+    return input('Digite "e" para entrar no menu ou "s" para sair: ').lower()
+
+def processar_opcao(caixa_eletronico, opcao_usuario):
+    if opcao_usuario == MENU_ENTRADA:
+        caixa_eletronico.menu()
+    elif opcao_usuario == MENU_SAIR:
+        exibir_mensagem_despedida()
+        return False
+    else:
+        print('Opção inválida. Tente novamente.')
+    return True
+
+def iniciar_caixa_eletronico(caixa_eletronico):
+    opcao_usuario = None
+    
+    while opcao_usuario != MENU_SAIR:
+        exibir_mensagem_boas_vindas()
+        opcao_usuario = obter_opcao_usuario()
+        if not processar_opcao(caixa_eletronico, opcao_usuario):
+            break
 
 def main():
-    caixa_eletronico = CaixaEletronico()
+    from controllers.atmControllers import CaixaEletronico 
+    caixa_eletronico = CaixaEletronico() 
     caixa_eletronico.iniciar_sessao() 
-    
-    resposta = ''
-    
-    while resposta != 's':
-        print("\nBem-vindo ao Caixa Eletrônico!")
-        resposta = input('Digite "e" para entrar no menu ou "s" para sair: ').lower()
-        
-        if resposta == 'e':
-            caixa_eletronico.menu()
-        elif resposta == 's':
-            print('Obrigado por usar o caixa eletrônico. Até logo!')
-        else:
-            print('Resposta inválida. Tente novamente.')
-            
+    iniciar_caixa_eletronico(caixa_eletronico) 
+
 if __name__ == "__main__":
     main()
